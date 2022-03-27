a5d1b6d6b4a2fb9dd3e8618b7edf844a1bc6f11e
chidalu okechukwu <73858189+Chidalu567@users.noreply.github.com>
2022-03-27 22:10:28 +1100

    Updated the NFTMarketplace contract and add Tranfer of ownership from the owner or contract to the buyer

--- contracts/NFTMarketPlace.sol
+++ contracts/NFTMarketPlace.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.6.12;
 
 // openzeppelin imports
 import "@openzeppelin/contracts/token/ERC721/ERC721";  // still the ERC721 standard
 import "@openzeppelin/contracts/utils/Counters.sol"; // Provides the utility from opensepplin for incrementation like id incrementation
-import "openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract NFTMarket is ReentrancyGuard{
     using Counters for Counters.Counter;
@@ -22,13 +22,13 @@ contract NFTMarket is ReentrancyGuard{
 
     // creating the marketItem object
     struct MarketItem{
-        uint256 _itemId; // the id of the item
-        uint256 _itemSold; // the id of the item sold
-        bool sold; // know if the item is sold already
-        address payable owner; // address of the owner
-        address payable seller; // address of the person selling
-        address nftContract; // address of our nftContract
         uint256 price; // price of the item
+        uint256 itemId; // the id of the item
+        uint256 tokenId; // the Id of the token
+        address payable ownerAddress; // address of the owner or whosoever bought the marketItem
+        address payable sellerAddress; // address of the person selling the marketItem which is the address of the contract
+        address nftContractAddress; // address of our nftContract
+        bool sold; // know if the item is sold already
     }
 
     // mapping an id to each marketItem created
@@ -36,17 +36,79 @@ contract NFTMarket is ReentrancyGuard{
     
     // call the event each time a marketitem is created
     event WhenMarketItemCreated(
-        uint256 price;
-        uint256 indexed _itemId;
-        uint256 indexed _itemSold;
-        address payable ownerAddress;
-        address payable sellerAddress;
-        address indexed nftContractAddress;
-        bool sold;
-    )
+        uint256 price,
+        uint256 indexed itemId,
+        uint256 indexed tokenId,
+        address payable ownerAddress,
+        address payable sellerAddress,
+        address indexed nftContractAddress,
+        bool sold
+    );
 
     // utility function to return the listing price
-    function getListingPrice() public view return uint256{
+    function getListingPrice() public view returns (uint256) {
         return listingPrice; // return the listing price
+    }
+
+    // Create MarketItem function.
+    // This function creates the marketItem and assign the change of ownership power to the contract
+    function createMarketItem (uint256 price, address nftContractAddress, uint256 tokenId) public payable nonReentrant {
+       
+       // validate that the price is greater than zero
+        require(price > 0,"Your price must be greater than Zero");
+       
+        // validate that the sender of the contract sends a value so that it is also transferred to whosoever buys it
+        require(msg.value == listingPrice,"The value must be equal to the listing Price");
+        
+        // Once the function is called we increment the tokenId so that we can mapp the marketItem created
+        _itemId.increment();
+        uint256 C_item_id = _itemId.current();
+        Id_To_MarketItem(C_item_id) = MarketItem(
+            price,
+            C_item_id,
+            tokenId,
+            payable(address(0)),
+            payable(msg.sender),
+            nftContractAddress,
+            false
+        );
+
+        // A statement that transfer the ownership of the NFT to the buyer
+        IERC721(nftContractAddress).transferFrom(msg.sender,address(this),tokenId);
+
+        // emit the event
+        emit WhenMarketItemCreated(
+            price,
+            C_item_id,
+            tokenId,
+            address(0),
+            msg.sender,
+            nftContractAddress,
+            false
+        );
+    }
+
+    // Function to call for a MArketSales
+    // A buyer buys an nft paying a price, we transfer the ownership
+    // from the contract to the buyer. We pay the creator of the marketplace a commission
+    function MarketSale(address nftContractAddress,uint256 itemId) public payable{
+        // get the price of nft and tokenId from the marketItem created.
+        uint256 Price_of_NFT = Id_To_MarketItem[itemId].price;
+        uint256 tokenId = Id_To_MarketItem[itemId].tokenId; 
+
+        // Make sure the  buyer pays the actual money to purchase the nft
+        require(msg.value == Price_of_NFT,"Amount must be equal to the amount of the NFT to purchase"); 
+        Id_To_MarketItem[itemId].seller.transfer(msg.value); // transfer the price the seller
+
+        // transfer ownership from contract to the buyer
+        IERC721(nftContractAddress).transferFrom(address(this),msg.sender,tokenId);
+
+        // set the owner of the nft to the buyer
+        Id_To_MarketItem[itemId].owner = payable(msg.sender);
+        
+        // set the sold nft boll to true after marketsales
+        Id_To_MarketItem[itemId].sold = true;
+
+        _itemSold.increment(); // increment the item sold
     }
 }
